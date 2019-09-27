a1846ae098ce59feb0a4bc162493336c4b4933f3
Nihagy <valdenor.michael@gmail.com>
2019-09-27 11:13:34 -0300

    finalizar interface

---
+++ script/script.js
@@ -0,0 +1,40 @@
+// let outros = document.querySelector(".inp_outros");
+// let ativar = document.querySelector(".camp_outros");
+// let selecionar = document.querySelector(".sobre_evento")
+
+
+// function mostrarCampo(){
+//     if(camp_outros.input.disabled("desbilitado")){
+//         camp_outros.input.remove("desabilitado");
+//     }else{
+//         camp_outros.input.add("visivel")
+//     }
+//     camp_outros.toggle("reload")
+// }
+
+
+
+
+var input= document.querySelector('.outra_opcao');
+var radio= document.querySelector('.inp_outros');
+
+
+function ativaCampo() {
+
+   
+    if (radio.checked == false)
+    {
+       input.disabled = disabled;
+    }
+    else if (radio.checked == true)
+    {
+       input.disabled = readonly;
+    }
+   }
+
+// function mostrarMenu(){
+
+//     nav.classList.toggle("visivel");
+//     menuHamburguer.classList.toggle("ativo");
+// }
+
